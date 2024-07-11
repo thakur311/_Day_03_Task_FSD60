fe94097d6680377a187ef0d7720e48074021c5f0
unknown <thakur311>
2024-07-11 10:52:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// How to compare two JSON have the same properties without order?
+// a. var obj1={nameL:"Person 1",age:5}
+// b. var obj2={name:"Person2", age:5}
+
+
+let obj1={
+    name:"Person1",
+    age:5
+}
+let obj2={
+    age:5,
+    name:"Person2"
+}
+
+if (obj1.key==obj2.key){
+    console.log("true")
+}
+else{
+    console.log("False")
+}
+
+
+//Use the rest countries API and display all the country flag in console.
+var req=new XMLHttpRequest();
+req.open('GET','https://restcountries.com/v3.1/all')
+req.send()
+req.onload=function(){
+var data=JSON.parse(req.response)
+var countries=data.map(country=>
+    console.log(country.flags.png))
+}
+ 
+// Use the same rset countries API and print all countries name, regions, sub-region and population
+var req=new XMLHttpRequest();
+req.open('GET','https://restcountries.com/v3.1/all')
+req.send()
+req.onload=function(){
+var data=JSON.parse(req.response)
+var countries=data.map(country=>
+console.log(`${country.name.common}, ${country.region},${country.subregion},${country.population}`))
+}
